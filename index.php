ac3f996d1be5868af2f4f53bd6b823d8d66abfce
Ian Hyzy <ianhyzy@gmail.com>
2014-12-09 21:57:59 -0600

    Fix

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>IanHyzy.tk</title>
+    <title>IanHyzy.me</title>
     <link rel="icon" type="image/png" href="/img/favicon.png">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
@@ -30,7 +30,7 @@
     <?php include 'navbar.php'; ?>
 
     <div class="jumbotron center">
-      <h1>Butts.</h1>
+      <h1>Create an awesome media server</h1>
       <p>With just a few hours, you can turn an old computer into a streaming machine.</p>
       <p><a class="btn btn-primary btn-lg" href="/projects/mediaserver/setup.php">More Information</a></p>
     </div>
