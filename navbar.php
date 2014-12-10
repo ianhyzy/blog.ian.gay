5720664b62f877c614725908c898a7b3d87ecf52
Ian Hyzy <ianhyzy@gmail.com>
2014-12-10 15:28:45 -0600

    Added blog, tweaked css

--- navbar.php
+++ navbar.php
@@ -1,8 +1,8 @@
 <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
   ga('create', 'UA-55629547-1', 'auto');
   ga('require', 'displayfeatures');
@@ -10,52 +10,53 @@
 </script>
 <!-- Quantcast Tag -->
 <script type="text/javascript">
-var _qevents = _qevents || [];
+  var _qevents = _qevents || [];
 
-(function() {
-var elem = document.createElement('script');
-elem.src = (document.location.protocol == "https:" ? "https://secure" : "http://edge") + ".quantserve.com/quant.js";
-elem.async = true;
-elem.type = "text/javascript";
-var scpt = document.getElementsByTagName('script')[0];
-scpt.parentNode.insertBefore(elem, scpt);
-})();
+  (function() {
+    var elem = document.createElement('script');
+    elem.src = (document.location.protocol == "https:" ? "https://secure" : "http://edge") + ".quantserve.com/quant.js";
+    elem.async = true;
+    elem.type = "text/javascript";
+    var scpt = document.getElementsByTagName('script')[0];
+    scpt.parentNode.insertBefore(elem, scpt);
+  })();
 
-_qevents.push({
-qacct:"p-0x5KvXV4TpMkA"
-});
+  _qevents.push({
+    qacct:"p-0x5KvXV4TpMkA"
+  });
 </script>
 
 <noscript>
-<div style="display:none;">
-<img src="//pixel.quantserve.com/pixel/p-0x5KvXV4TpMkA.gif" border="0" height="1" width="1" alt="Quantcast"/>
-</div>
+  <div style="display:none;">
+    <img src="//pixel.quantserve.com/pixel/p-0x5KvXV4TpMkA.gif" border="0" height="1" width="1" alt="Quantcast"/>
+  </div>
 </noscript>
 <!-- End Quantcast tag -->
 <div class="navbar navbar-default">
-    <div class="navbar-header">
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-        </button>
-        <a class="nav navbar-brand" href="/index.php">IanHyzy.me</a>
-    </div>
-    <div class="navbar-collapse collapse navbar-responsive-collapse">
-        <ul class="nav navbar-nav">
-            <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-flask"></i>Projects<b class="caret"></b></a>
-                <ul class="dropdown-menu">
-                    <!--<li class="nav-list-item"><a href="/projects/mediaserver/setup.php"><i class="fa fa-linux fa-fw"></i>Media server</a>
-                    </li>-->
-                  <li class="nav-list-item"><a href="/projects/roomba/roomba.php"><i class="fa fa-linux fa-fw"></i>Roomba Drive</a>
-                    </li>
-                  <!-- <li class="nav-list-item"><a href="/projects/cabbage.php"><i class="fa fa-cc fa-fw"></i>Cabbage Clicker</a>
-                    </li> -->
-                </ul>
-            </li>
-            <li><a href="/aboutme.php"><i class="fa fa-comment"></i>About Me</a>
-            </li>
+  <div class="navbar-header">
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <a class="nav navbar-brand" href="/index.php">IanHyzy.me</a>
+  </div>
+  <div class="navbar-collapse collapse navbar-responsive-collapse">
+    <ul class="nav navbar-nav">
+      <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-flask"></i>Projects<b class="caret"></b></a>
+        <ul class="dropdown-menu">
+          <!--<li class="nav-list-item"><a href="/projects/mediaserver/setup.php"><i class="fa fa-linux fa-fw"></i>Media server</a>
+</li>-->
+          <li class="nav-list-item"><a href="/projects/roomba/roomba.php"><i class="fa fa-linux fa-fw"></i>Roomba Drive</a>
+          </li>
+          <!-- <li class="nav-list-item"><a href="/projects/cabbage.php"><i class="fa fa-cc fa-fw"></i>Cabbage Clicker</a>
+</li> -->
         </ul>
-    </div>
+      </li>
+      <li><a href="/aboutme.php"><i class="fa fa-comment"></i>About Me</a>
+      <li><a href="blog.php"><i class="fa fa-newspaper-o"></i>Blog</a></li>
+      </li>
+    </ul>
+</div>
 </div>
