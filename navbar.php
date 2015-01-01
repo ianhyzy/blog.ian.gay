b01f10fc48233da00ced344b5e3aa043ed711563
Ian Hyzy <ianhyzy@gmail.com>
2015-01-01 12:00:52 -0600

    Updated Google Analytics

--- navbar.php
+++ navbar.php
@@ -1,12 +1,12 @@
 <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-                          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-55629547-1', 'auto');
-  ga('require', 'displayfeatures');
+  ga('create', 'UA-58147107-1', 'auto');
   ga('send', 'pageview');
+
 </script>
 <!-- Quantcast Tag -->
 <script type="text/javascript">
