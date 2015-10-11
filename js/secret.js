81db4f9ebe519aaabb19e2bcff91fe8ce42f84bc
Ian Hyzy <ianhyzy@gmail.com>
2015-10-11 18:06:46 +0200

    added images, fixed homepage, organization

--- js/secret.js
+++ js/secret.js
@@ -1,4 +1,6 @@
-var listener = new window.keypress.Listener();
-listener.sequence_combo("up up down down left right left right b a enter", function() {
-    swag_mode = true;
-});
+function press() {
+  var listener = new window.keypress.Listener();
+  listener.sequence_combo("up up down down left right left right b a enter", function() {
+    console.log("THERE WILL BE A SECRET HERE EVENTUALLY!");
+  });
+}
