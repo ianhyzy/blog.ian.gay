3e3c1f72ec0121b4f0ac40c13f41d0fe47b26c2c
Ian Hyzy <ianhyzy@gmail.com>
2016-01-13 18:46:33 +0100

    gravity

--- js/secret.js
+++ js/secret.js
@@ -1,6 +1,6 @@
 function press() {
   var listener = new window.keypress.Listener();
-  listener.sequence_combo("up up down down left right left right b a enter", function() {
-    console.log("THERE WILL BE A SECRET HERE EVENTUALLY!");
+  listener.sequence_combo("b a enter", function() {
+    $('body').jGravity();
   });
 }
