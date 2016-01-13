c032e188554abfe64dab8fd1717eae2dd488414d
Ian Hyzy <ianhyzy@gmail.com>
2016-01-13 19:30:26 +0100

    remove easter egg and dependencies

--- js/secret.js
+++
@@ -1,6 +0,0 @@
-function press() {
-  var listener = new window.keypress.Listener();
-  listener.sequence_combo("b a enter", function() {
-    console.log("There was going to be some big event here. Gravity would turn on, dumping the photos everywhere, or patterns would fly across the screen, or something else dramatic. You get the idea.");
-  });
-}
